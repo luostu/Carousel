1ab290171b8a93b167adb5d0239062d88c61e56e
luostu <935663182@qq.com>
2019-12-10 20:55:47 +0800

    second commit

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-let n = 1
+
+let n
+initialization()
 setInterval(()=>{
-  $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+  makeLeave(getImage(n))
     .one('transitionend', (e)=>{
-      $(e.currentTarget).removeClass('leave').addClass('enter')
+      makeEnter($(e.currentTarget))
     })
-  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+  makeCurrent(getImage(n+1))
   n += 1
 },3000)
 
 
+
+
+function getImage(n){
+    return $(`.images > img:nth-child(${x(n)})`)
+}
 function x(n){
   if(n>3){
     n = n%3
@@ -20,4 +24,19 @@ function x(n){
     }
   } // n = 1 2 3
   return n
+}
+
+function initialization(){
+    n=1
+    $(`.images > img:nth-child(${x(n)})`).addClass('current')
+    .siblings().addClass('enter')
+}
+function makeCurrent($node){
+return $node.removeClass('enter current').addClass('current')
+}
+function makeLeave($node) {
+    return $node.removeClass('current enter').addClass('leave')
+}
+function makeEnter ($node) {
+    return $node.removeClass('leave current').addClass('enter')
 }
